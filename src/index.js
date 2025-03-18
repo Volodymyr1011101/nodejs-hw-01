4cebb3136adf33c6ad83339eac16a0786e1541ed
Volodymyr Voronkin <voronkinvladimir328@gmail.com>
2025-03-18 23:12:37 +0100

    first homework

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { readContacts } from './utils/readContacts.js';
-import { generateContacts } from './scripts/generateContacts.js';
-const mainFn = async ()  => {
-    await generateContacts(5);
-    console.log(await readContacts());
-};
-mainFn();
